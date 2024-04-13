2d6e90890584f0fdf27435ee6ac45c7638d8ddfe
alstjr5949 <alstjr5949@gmail.com>
2024-04-13 20:21:40 +0900

    feat: firebase init

---
+++ src/routes/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4thgw2EGKJIgXkvevcvtzayrUQrDZQvY",
+  authDomain: "twitter-clone-2e2e7.firebaseapp.com",
+  projectId: "twitter-clone-2e2e7",
+  storageBucket: "twitter-clone-2e2e7.appspot.com",
+  messagingSenderId: "579295206856",
+  appId: "1:579295206856:web:adeb8a430234b1a4afb4fd",
+};
+
+const app = initializeApp(firebaseConfig);
