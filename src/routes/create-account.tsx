d0cca98c5bb02a35fe9c871a5e40a59fe7549ddd
alstjr5949 <alstjr5949@gmail.com>
2024-04-13 21:11:59 +0900

    feat: firebase login

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,3 +1,135 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { auth } from "./firebase";
+import { useNavigate } from "react-router-dom";
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 50px 0;
+`;
+
+const Title = styled.h1`
+  font-size: 42px;
+`;
+
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+
+  &[type="submit"] {
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+  margin-top: 10px;
+`;
+
 export default function CreateAccount() {
-  return <h1>create account!!</h1>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "name") {
+      setName(value);
+    } else if (name === "password") {
+      setPassword(value);
+    } else if (name === "email") {
+      setEmail(value);
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (isLoading || !name || !email || !password) return;
+
+    try {
+      setIsLoading(true);
+      // create an account
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(credentials.user);
+
+      // set the name of the user
+      await updateProfile(credentials.user, { displayName: name });
+      // redirect to the home
+      navigate("/");
+    } catch (e) {
+      // set Error
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Title>Join 𝕏</Title>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          onChange={handleChange}
+          name="name"
+          value={name}
+          placeholder="Name"
+          type="text"
+          required
+        />
+        <Input
+          onChange={handleChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required
+        />
+        <Input
+          onChange={handleChange}
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input
+          type="submit"
+          value={isLoading ? "Loading..." : "Create Account"}
+        />
+      </Form>
+      {!!errorMessage && <Error>{errorMessage}</Error>}
+    </Wrapper>
+  );
 }
