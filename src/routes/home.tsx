0c750b8e3467e4136c74b816f3967c352f259ed1
alstjr5949 <alstjr5949@gmail.com>
2024-04-14 10:28:07 +0900

    feat: tweet input ui

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,17 +1,12 @@
-import { useNavigate } from "react-router-dom";
-import { auth } from "./firebase";
+import PostTweetForm from "../components/post-tweet-form";
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
 
 export default function Home() {
-  const navigate = useNavigate();
-
-  const logOut = () => {
-    auth.signOut();
-    navigate("/login");
-  };
-
   return (
-    <h1>
-      <button onClick={logOut}>Log Out</button>
-    </h1>
+    <Wrapper>
+      <PostTweetForm />
+    </Wrapper>
   );
 }
